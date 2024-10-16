f1247fede1c47f8d3858b11e23e4a8c75413e0e4
Mohamed Osama <mohamed.o.hassan98@outlook.com>
2024-10-16 23:32:19 +0300

    chore: add Swagger setup for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Voucher API')
+    .setDescription('The voucher API description')
+    .setVersion('1.0')
+    .addTag('customers')
+    .addTag('vouchers')
+    .addTag('special-offers')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(4000);
 }
 bootstrap();
